5b4e432d5fe4854ce70d2ae8fd9c9e1f066d16da
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-16 16:30:30 +0100

    Web API Controllers & Model Binding

---
+++ T02_WebAPIControllers/Controllers/ShirtsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T02_WebAPIControllers.Models;
+
+namespace T02_WebAPIControllers.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class ShirtsController : ControllerBase
+	{
+		[HttpPost]
+		public string CreateShirt([FromBody] ShirtModel shirt)
+		{
+			return $"Create a shirt.";
+		}
+
+		[HttpGet]
+		public string ReadAllShirts()
+		{
+			return $"Read all shirts.";
+		}
+
+		[HttpGet("{id}")]
+		public string ReadShirtById(int id,
+			[FromQuery] string primaryColor, [FromHeader] string secondaryColor)
+		{
+			return $"Read a shirt with Id: {id}, primary color: {primaryColor}, secondary color: {secondaryColor}.";
+		}
+
+		[HttpPut("{id}")]
+		public string UpdateShirt(int id)
+		{
+			return $"Update a shirt with Id: {id}.";
+		}
+
+		[HttpDelete("{id}")]
+		public string DeleteShirt(int id)
+		{
+			return $"Delete a shirt with Id: {id}.";
+		}
+	}
+}
