d7123f2d4f87edfd626a0d6d78c3f317416ac74c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-17 13:21:26 +0100

    Web API Return types

--- T02_WebAPIControllers/Controllers/ShirtsController.cs
+++ T02_WebAPIControllers/Controllers/ShirtsController.cs
@@ -7,35 +7,85 @@ namespace T02_WebAPIControllers.Controllers
 	[Route("api/v1/[controller]")]
 	public class ShirtsController : ControllerBase
 	{
+		private List<ShirtModel> shirts = [
+			new ShirtModel
+			{
+				ShirtId = 1,
+				Brand = "My Brand",
+				Color = "Blue",
+				Gender = "Men",
+				Price = 30,
+				Size = 10
+			},
+			new ShirtModel
+			{
+				ShirtId = 2,
+				Brand = "My Brand",
+				Color = "Black",
+				Gender = "Men",
+				Price = 35,
+				Size = 12
+			},
+			new ShirtModel
+			{
+				ShirtId = 3,
+				Brand = "Your Brand",
+				Color = "Pink",
+				Gender = "Women",
+				Price = 28,
+				Size = 8
+			},
+			new ShirtModel
+			{
+				ShirtId = 4,
+				Brand = "Your Brand",
+				Color = "Yello",
+				Gender = "Women",
+				Price = 30,
+				Size = 9
+			}
+		];
+
 		[HttpPost]
-		public string CreateShirt([FromBody] ShirtModel shirt)
+		public IActionResult CreateShirt([FromBody] ShirtModel shirt)
 		{
-			return $"Create a shirt.";
+			return Ok($"Create a shirt.");
 		}
 
 		[HttpGet]
-		public string ReadAllShirts()
+		public IActionResult ReadAllShirts()
 		{
-			return $"Read all shirts.";
+			return Ok($"Read all shirts.");
 		}
 
 		[HttpGet("{id}")]
-		public string ReadShirtById(int id,
-			[FromQuery] string primaryColor, [FromHeader] string secondaryColor)
+		public IActionResult ReadShirtById(int id)
 		{
-			return $"Read a shirt with Id: {id}, primary color: {primaryColor}, secondary color: {secondaryColor}.";
+			if (id < 1)
+			{
+				return BadRequest();
+			}
+
+			var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+
+			if (shirt == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(shirt);
 		}
 
 		[HttpPut("{id}")]
-		public string UpdateShirt(int id)
+		public IActionResult UpdateShirt(int id)
 		{
-			return $"Update a shirt with Id: {id}.";
+			return Ok($"Update a shirt with Id: {id}.");
 		}
 
 		[HttpDelete("{id}")]
-		public string DeleteShirt(int id)
+		public IActionResult DeleteShirt(int id)
 		{
-			return $"Delete a shirt with Id: {id}.";
+			return Ok($"Delete a shirt with Id: {id}.");
 		}
 	}
 }
