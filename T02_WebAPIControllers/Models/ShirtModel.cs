a619f7e67fac959095dfb2821104d7ef0d62ba32
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-12-17 12:59:20 +0100

    Model Validation with DataAnnatation & ValidationAttribute

--- T02_WebAPIControllers/Models/ShirtModel.cs
+++ T02_WebAPIControllers/Models/ShirtModel.cs
@@ -1,12 +1,24 @@
-﻿namespace T02_WebAPIControllers.Models
+﻿using System.ComponentModel.DataAnnotations;
+using T02_WebAPIControllers.Models.Validations;
+
+namespace T02_WebAPIControllers.Models
 {
 	public class ShirtModel
 	{
 		public int ShirtId { get; set; }
+
+		[Required]
 		public string? Brand { get; set; }
+
+		[Required]
 		public string? Color { get; set; }
-		public int Size { get; set; }
+
+		[Shirt_EnsureCorrectSizing]
+		public int? Size { get; set; }
+
+		[Required]
 		public string? Gender { get; set; }
-		public double Price { get; set; }
+
+		public double? Price { get; set; }
 	}
 }
---
+++ T02_WebAPIControllers/Models/Validations/Shirt_EnsureCorrectSizingAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T02_WebAPIControllers.Models.Validations
+{
+	public class Shirt_EnsureCorrectSizingAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (validationContext.ObjectInstance is ShirtModel shirt &&
+				!string.IsNullOrWhiteSpace(shirt.Gender))
+			{
+				if (shirt.Gender.Equals("men", StringComparison.OrdinalIgnoreCase) && shirt.Size < 8)
+				{
+					return new
+						ValidationResult("For men's shirts, the size has to be greater or equal to 8.");
+				}
+				else if (shirt.Gender.Equals("women", StringComparison.OrdinalIgnoreCase) && shirt.Size < 6)
+				{
+					return new
+						ValidationResult("For women's shirts, the size has to be greater or equal to 6.");
+				}
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
