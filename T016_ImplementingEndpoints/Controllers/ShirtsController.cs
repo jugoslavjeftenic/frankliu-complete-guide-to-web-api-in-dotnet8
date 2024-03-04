050d19137eb5719d4d255291342ef17134614df2
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-03-05 00:32:50 +0100

    Last section setup

---
+++ T016_ImplementingEndpoints/Controllers/ShirtsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T016_ImplementingEndpoints.Filters.ActionFilters;
+using T016_ImplementingEndpoints.Models;
+using T016_ImplementingEndpoints.Models.Repositories;
+
+namespace T016_ImplementingEndpoints.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class ShirtsController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetShirts()
+		{
+			return Ok("Reading all the shirts.");
+		}
+
+		[HttpGet("{id}")]
+		[Shirt_ValidateShirtIdFilter]
+		public IActionResult GetShirtById(int id)
+		{
+			return Ok(ShirtRepository.GetShirtById(id));
+		}
+
+		[HttpGet("routeColor/{color}")]
+		public IActionResult GetShirtByColorFromRoute([FromRoute] string color)
+		{
+			return Ok($"Reading shirt with color: {color}.");
+		}
+
+		[HttpGet("queryColor")]
+		public IActionResult GetShirtByColorFromQuery([FromQuery] string color)
+		{
+			return Ok($"Reading shirt with color: {color}.");
+		}
+
+		[HttpGet("headerColor")]
+		public IActionResult GetShirtByColorFromHeader([FromHeader(Name = "Color")] string color)
+		{
+			return Ok($"Reading shirt with color: {color}.");
+		}
+
+		[HttpPost]
+		public IActionResult CreateShirt([FromBody] ShirtModel shirt)
+		{
+			return Ok($"Creating a shirt ({shirt.Brand}, {shirt.Color}).");
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateShirt(int id, [FromForm] ShirtModel shirt)
+		{
+			return Ok($"Updating shirt with id: {id} ({shirt.Brand}, {shirt.Color}).");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteShirt(int id)
+		{
+			return Ok($"Deleting shirt with id: {id}.");
+		}
+	}
+}
---
+++ T016_ImplementingEndpoints/Models/ShirtModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using T016_ImplementingEndpoints.Models.Validations;
+
+namespace T016_ImplementingEndpoints.Models
+{
+	public class ShirtModel
+	{
+		public int ShirtId { get; set; }
+
+		[Required]
+		public string Brand { get; set; } = "";
+
+		[Required]
+		public string Color { get; set; } = "";
+
+		[Shirt_EnsureCorrectSizing]
+		public int? Size { get; set; }
+
+		[Required]
+		public string Gender { get; set; } = "";
+
+		public double? Price { get; set; }
+	}
+}
