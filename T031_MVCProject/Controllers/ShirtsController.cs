3578637387fb5b7318043bd09ac09f879e83762b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-03-10 13:58:57 +0100

    Update Shirt View

--- T031_MVCProject/Controllers/ShirtsController.cs
+++ T031_MVCProject/Controllers/ShirtsController.cs
@@ -10,7 +10,7 @@ namespace T031_MVCProject.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			return View(await _webApiExecutor.InvokeGet<List<ShirtModel>>("Shirts"));
+			return View(await _webApiExecutor.InvokeGet<List<ShirtModel>>("shirts"));
 		}
 
 		public IActionResult CreateShirt()
@@ -23,7 +23,7 @@ namespace T031_MVCProject.Controllers
 		{
 			if (ModelState.IsValid)
 			{
-				var response = await _webApiExecutor.InvokePost("Shirts", shirt);
+				var response = await _webApiExecutor.InvokePost("shirts", shirt);
 				if (response is not null)
 				{
 					return RedirectToAction(nameof(Index));
@@ -31,6 +31,17 @@ namespace T031_MVCProject.Controllers
 			}
 
 			return View(shirt);
+		}
+
+		public async Task<IActionResult> UpdateShirt(int shirtId)
+		{
+			var shirt = await _webApiExecutor.InvokeGet<ShirtModel>($"shirts/{shirtId}");
+			if (shirt is not null)
+			{
+				return View(shirt);
+			}
+
+			return NotFound();
 		}
 	}
 }
