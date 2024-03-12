87fbdb9f6dc107298421e6e56e2b60f6d26f7b5c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-03-12 05:02:25 +0100

    Authenticate apps against Authority

---
+++ T045_WebApiSecurity/Authority/AppCredential.cs
@@ -0,0 +1,8 @@
+﻿namespace T045_WebApiSecurity.Authority
+{
+	public class AppCredential
+	{
+		public string ClientId { get; set; } = String.Empty;
+		public string Secret { get; set; } = String.Empty;
+	}
+}
---
+++ T045_WebApiSecurity/Controllers/AuthorityController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T045_WebApiSecurity.Authority;
+
+namespace T045_WebApiSecurity.Controllers
+{
+	[ApiController]
+	public class AuthorityController : ControllerBase
+	{
+		[HttpPost("auth")]
+		public IActionResult Authenticate([FromBody] AppCredential credential)
+		{
+			if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
+			{
+				return Ok(new
+				{
+					acces_token = CreateToken(credential.ClientId),
+					expires_at = DateTime.UtcNow.AddMinutes(10)
+				});
+			}
+			else
+			{
+				ModelState.AddModelError("Unauthorized", "You are not authorized.");
+				var problemDetails = new ValidationProblemDetails(ModelState)
+				{
+					Status = StatusCodes.Status401Unauthorized
+				};
+
+				return new UnauthorizedObjectResult(problemDetails);
+			}
+		}
+
+		private string CreateToken(string clientId)
+		{
+			return string.Empty;
+		}
+	}
+}
