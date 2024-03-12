ce068adfe10f2a270988cc900da77154f7d8d182
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-03-12 04:39:43 +0100

    Register apps against Authority

---
+++ T045_WebApiSecurity/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace T045_WebApiSecurity.Authority
+{
+	public static class AppRepository
+	{
+		private static List<Application> _applications =
+		[
+			new Application
+			{
+				ApplicationId = 1,
+				ApplicationName = "MVCWebApp",
+				ClientId = "53D3C1E6-4587-4AD5-8C6E-A8E4BD59940E",
+				Secret = "0673FC70-0514-4011-B4A3-DF9BC03201BC"
+			}
+		];
+
+		public static bool Authenticate(string clientId, string secret)
+		{
+			return _applications.Any(x => x.ClientId.Equals(clientId) && x.Secret.Equals(secret));
+		}
+
+		public static Application? GetApplicationByClientId(string clientId)
+		{
+			return _applications.FirstOrDefault(x => x.ClientId.Equals(clientId));
+		}
+	}
+}
---
+++ T045_WebApiSecurity/Authority/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace T045_WebApiSecurity.Authority
+{
+	public class Application
+	{
+		public int ApplicationId { get; set; }
+		public string ApplicationName { get; set; } = String.Empty;
+		public string ClientId { get; set; } = String.Empty;
+		public string Secret { get; set; } = String.Empty;
+	}
+}
