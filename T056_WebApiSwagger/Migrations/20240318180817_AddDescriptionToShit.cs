6e8ce917e2d634ed7f87425008671fb501c94699
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-03-18 19:13:42 +0100

    Run migration to add new field

---
+++ T056_WebApiSwagger/Migrations/20240318180817_AddDescriptionToShit.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace T056_WebApiSwagger.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToShit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.AddColumn<string>(
+		        name: "Description",
+		        table: "Shirts",
+		        nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shirts");
+        }
+    }
+}
